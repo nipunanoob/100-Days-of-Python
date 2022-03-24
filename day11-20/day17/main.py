eb6af1c9143ac5f59776dc4a5de6b03b7999c1fb
Nipun Anoob <nipunanoob@gmail.com>
2022-03-24 18:32:14 +0530

    Added day17 folder

---
+++ day11-20/day17/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    text = question['question']
+    answer = question['correct_answer']
+    question_bank.append(Question(text, answer))
+
+quiz_brain = QuizBrain(question_bank)
+while quiz_brain.still_has_question():
+    quiz_brain.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was {quiz_brain.score}/{quiz_brain.question_number}")
+
