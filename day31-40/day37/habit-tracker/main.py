96a15761e27577c54d4b9d38c2684271afce54a2
Nipun Anoob <nipunanoob@gmail.com>
2022-07-18 15:19:52 +0530

    Added sleep tracker script

--- day31-40/day37/habit-tracker/main.py
+++ day31-40/day37/habit-tracker/main.py
@@ -1,19 +1,67 @@
-from urllib import response
 import requests
+from datetime  import date,timedelta
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 pixela_endpoint = "https://pixe.la/v1/users"
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('PIXELA_TOKEN')
+USERNAME = os.getenv('PIXELA_USERNAME')
 user_params = {
     'token': TOKEN,
-    'username': 'ginzuka',
+    'username': USERNAME,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes'
 }
 
 # response = requests.post(url=pixela_endpoint,json=user_params)
 # print(response.text)
+pixela_graph_endpoint= f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_id = "graph1"
+graph_config = {
+    "id": graph_id,
+    "name": 'Sleeping graph',
+    "unit": "Hours",
+    'type': "float",
+    "color": "ajisai"
+}
+request_headers = {
+    'X-USER-TOKEN': TOKEN
+}
+# response = requests.post(url=pixela_graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+# today = date.today()
+# yesterday = (today - timedelta(1)).strftime("%Y%m%d")
+
+today = date.today().strftime("%Y%m%d")
+
+
+
+selected_graph_endpoint = f"{pixela_graph_endpoint}/{graph_id}"
+selected_graph_add_pixel_config = {
+    'date': today,
+    'quantity': input("How mnay hours did you sleep yesterday?")
+}
+
+#Creates pixel
+response = requests.post(url=selected_graph_endpoint, json=selected_graph_add_pixel_config, headers=request_headers)
+print(response.text)
+
+# selected_graph_update_pixel_config = {
+#     'quantity': "6"
+# }
+
+#Updates pixel
+# print(f"{selected_graph_endpoint}/{today}")
+# response = requests.put(url = f"{selected_graph_endpoint}/{today}",json= selected_graph_update_pixel_config,
+#                                     headers=request_headers)
+
+# print(response.text)
+
+#Deletes pixel
+# response = requests.delete(url=f"{selected_graph_endpoint}/{yesterday}", headers=request_headers)
+# print(response.text)
+
 
